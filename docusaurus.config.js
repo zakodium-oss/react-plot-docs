3a235deff555f317a154b6c221a5583105569dff
Michaël Zasso <targos@protonmail.com>
2022-11-01 14:37:38 +0100

    chore: update dependencies

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -73,7 +73,6 @@ const config = {
             require.resolve('./src/css/custom.css'),
             require.resolve('./src/css/demo.css'),
           ],
-          prism: {},
         },
       }),
     ],
